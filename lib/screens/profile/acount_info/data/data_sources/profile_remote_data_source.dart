0fb9308692e30a42b526acd2e01e0951472d726e
doniaAlaa <doniaalaa1095@gmail.com>
2026-07-27 18:49:39 +0300

    connection

---
+++ lib/screens/profile/acount_info/data/data_sources/profile_remote_data_source.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:scraapy_pro/core/config/res/constants_manager.dart';
+import 'package:scraapy_pro/core/helpers/cache_service.dart';
+import 'package:scraapy_pro/core/storage/session_storage_keys.dart';
+
+abstract class ProfileRemoteDataSource {
+  Future<Map<String, dynamic>> fetchMe();
+}
+
+
+class ProfileRemoteDataSourceImpl implements ProfileRemoteDataSource {
+  static Uri _u(String path) => Uri.parse('${ConstantManager.baseUrl2}$path');
+
+  @override
+  Future<Map<String, dynamic>> fetchMe() async {
+    final token = await SecureStorage.read(SessionStorageKeys.token);
+    if (token == null || token.isEmpty) {
+      throw Exception('unauthorized');
+    }
+    final res = await http.get(
+      _u('users/me/'),
+      headers: {'Authorization': 'Token $token'},
+    );
+    if (res.statusCode != 200) {
+      throw Exception('user_load_failed');
+    }
+    return json.decode(res.body) as Map<String, dynamic>;
+  }
+}
